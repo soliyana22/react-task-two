439c10c8a69a1efe091b51c120508ded4757c37c
Soliyana Yalewdeg <soliyanayalew22@gmail.com>
2025-06-23 18:12:46 +0300

    commited

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Landing.css';
-
+import { Header } from '../components/Header/Header';
 export const Landing = () => {
   return (
+    
     <div className="landing-page">
       <div className="container">
         {/* Header Section */}
